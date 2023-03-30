5496a916c92d6ee3d8963ad7c468bd5926156a05
MorpexX <morpexx08@gmail.com>
2023-03-30 16:17:53 +0300

    Добавьте файлы проекта.

---
+++ SVOIA_IGRA/GameMode/GameMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVOIA_IGRA.GameMode
+{
+    public class GameMode
+    {
+        public enum ModeEnum
+        {
+            Welcome,
+            Start,
+            Close,
+            Game
+        }
+    }
+}
---
+++ SVOIA_IGRA/GameMode/MyOwnGameViewModel.Navigator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SVOIA_IGRA.GameMode.GameMode;
+
+namespace SVOIA_IGRA.GameMode
+{
+    public partial class MyOwnGameMode
+    {
+        private ModeEnum mode;
+
+        public ModeEnum Mode
+        {
+            get => mode;
+
+            private set
+            {
+                mode= value;
+            }
+        }
+    }
+}
