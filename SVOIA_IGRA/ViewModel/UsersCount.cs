9f8e862af8dad5110bab3609da54f5ae6512ff8a
MorpexX <morpexx08@gmail.com>
2023-03-30 18:16:59 +0300

    Добавление команд валидации

--- SVOIA_IGRA/ViewModel/UsersCount.cs
+++ SVOIA_IGRA/ViewModel/UsersCount.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace SVOIA_IGRA.ViewModel
 {
-    internal class UsersCount
+    public class UsersCount : ValidationBase
     {
+        private int _Ucount;
+
+        public int UCount
+        {
+            get => _Ucount;
+
+            set
+            {
+                _Ucount = value;
+                HashError = value < 2 && value > 8;
+                if (HashError)
+                    throw new ArgumentOutOfRangeException(nameof(UCount));
+            }
+        }
+        public bool HashError { get; private set; } = true;
     }
 }
